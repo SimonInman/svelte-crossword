b32253afb24d7d5d68d2d374f26c0ebbe8b834f2
Austin Platt <austinplatt@gmail.com>
2023-09-28 20:18:28 +0100

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "csolve svelte frontend";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.05";
+    flake-utils.url = github:numtide/flake-utils;
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    with flake-utils.lib; eachSystem allSystems (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        # defaultPackage = packages.document;
+        devShell = pkgs.mkShell {
+          # development environment
+          buildInputs = [
+            pkgs.nodejs_20
+          ];
+        };
+      });
+}
